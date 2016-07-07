55a7c3112d1c75eea907f55418549038287df69e
Rick Anderson <rianders@docs.rutgers.edu>
2016-07-07 13:06:55 -0400

    Added messaging from client to  server. Updated temp code.

--- server-io.js
+++ server-io.js
@@ -16,73 +16,76 @@ var Ledstrip = require("./ledstrip.js");
 var ledstrip = new Ledstrip(board);
 var pixels = 30;
 var pixel_cur = 0;
+var LowAlarm;
 
-board.on("ready", function() {
-  ledstrip.hello();
-  ledstrip.clear();
-  ledstrip.show();
-  ledstrip.setPixelColor();
-  ledstrip.hello();
+//Socket connection handler
+io.on('connection', function(socket) {
+  console.log(socket.id);
+
+  board.on("ready", function() {
+    ledstrip.hello();
+    ledstrip.clear();
+    ledstrip.show();
+    ledstrip.setPixelColor();
+    ledstrip.hello();
 
     led = new five.Led(PIN_LED1);
     button = new five.Button({
-        pin: PIN_BTN1,
-        invert: true
+      pin: PIN_BTN1,
+      invert: true
     });
     // "down" the button is pressed
     button.on("down", function() {
-        btnStatus = "down";
-        led.on();
-        ledstrip.setPixelColor(pixel_cur++, 255, 0, 0);
-        ledstrip.show();
-        if (pixel_cur >= 30) {
-          pixel_cur = 0;
-          ledstrip.clear();
-        }
-        console.log("down");
+      btnStatus = "down";
+      led.on();
+      ledstrip.setPixelColor(pixel_cur++, 255, 0, 0);
+      ledstrip.show();
+      if (pixel_cur >= 30) {
+        pixel_cur = 0;
+        ledstrip.clear();
+      }
+      console.log("down");
     });
 
     // "hold" the button is pressed for specified time.
     button.on("hold", function() {
-        btnStatus = "hold";
-        console.log("hold");
+      btnStatus = "hold";
+      console.log("hold");
     });
 
     // "up" the button is released
     button.on("up", function() {
-        btnStatus = "up";
-        console.log("up");
+      btnStatus = "up";
+      console.log("up");
     });
 
     temp = new five.Sensor({
-        pin: "A0",
-        freq: 250,
-        threshold: 5
+      pin: "A0",
+      freq: 250,
+      threshold: 5
     });
     // When the sensor value changes, log the value
     temp.on("change", function(value) {
-        tempvalue = value * 330 / 1024;
-        console.log(value * 330 / 1024);
+      tempvalue = value * 330 / 1024;
+      console.log(tempValue);
+      socket.emit('emitting', {curTemp: tempValue});
     });
-    console.log("Board is Ready!");
-});
 
-//Socket connection handler
-io.on('connection', function(socket) {
-    console.log(socket.id);
+    console.log("Board is Ready!");
+
 
     socket.on('led:on', function(data) {
-        led.on();
-        console.log('LED ON RECEIVED');
+      led.on();
+      console.log('LED ON RECEIVED');
     });
     socket.on('led:off', function(data) {
-        led.off();
-        console.log('LED OFF RECEIVED');
+      led.off();
+      console.log('LED OFF RECEIVED');
 
     });
     socket.on('ledstrip:clear', function(data) {
       ledstrip.clear();
-        console.log('LEDSTRIP CLEAR RECEIVED');
+      console.log('LEDSTRIP CLEAR RECEIVED');
 
     });
     socket.on('ledstrip:show', function(data) {
@@ -97,56 +100,62 @@ io.on('connection', function(socket) {
     });
     socket.on('ledstrip:setpixelcolor', function(data) {
       ledstrip.setPixelColor(data.pixel, data.red, data.green, data.blue);
-      console.log('LEDSTRIP SETPIXELCOLOR pixel: %d, red: %d, green: %d, blue: %d',data.pixel, data.red, data.green, data.blue);
+      console.log('LEDSTRIP SETPIXELCOLOR pixel: %d, red: %d, green: %d, blue: %d', data.pixel, data.red, data.green, data.blue);
     });
+    socket.on('temp:setLowAlarm', function(data) {
+      LowAlarm = data;
+      console.log('Low Alarm for Temperature set: %d', data);
+      //socket.emit('lowalarmset', 'true');
+    });
+  });
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 
 app.get('/led/on', function(req, res) {
-    if (board.isReady) {
-        led.on();
-        res.send('led on!');
-    } else {
-        res.status(status).send("Board not ready");
-    }
+  if (board.isReady) {
+    led.on();
+    res.send('led on!');
+  } else {
+    res.status(status).send("Board not ready");
+  }
 
 });
 app.get('/led/off', function(req, res) {
-    if (board.isReady) {
-        led.off();
-        res.send('led off!');
-    } else {
-        res.status(status).send("Board not ready");
-    }
+  if (board.isReady) {
+    led.off();
+    res.send('led off!');
+  } else {
+    res.status(status).send("Board not ready");
+  }
 
 });
 app.get('/temp', function(req, res) {
-    var val = "not ready";
-    if (board.isReady) {
-        val = tempvalue;
-        //res.status(status).send(body);
-        res.send('Temperature:' + val);
-    } else {
-        res.status(status).send("Board not ready");
-    }
+  var val = "not ready";
+  if (board.isReady) {
+    val = tempvalue;
+    //res.status(status).send(body);
+    res.send('Temperature:' + val);
+  } else {
+    res.status(status).send("Board not ready");
+  }
 
 });
 app.get('/button', function(req, res) {
 
-    if (board.isReady) {
-        res.send('Button:' + btnStatus);
-    } else {
-        res.status(status).send("button not ready");
-    }
+  if (board.isReady) {
+    res.send('Button:' + btnStatus);
+  } else {
+    res.status(status).send("button not ready");
+  }
 });
 
 //server.listen(3000, "0.0.0.0", function() {
 server.listen(3000, function() {
-    console.log('Listening on: http://localhost:3000');
+  console.log('Listening on: http://localhost:3000');
 });
