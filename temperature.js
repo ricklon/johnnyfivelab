c0ac2f1b4c3369c723a588c4ab74e6bb290f871a
ricklon <rick.rickanderson@gmail.com>
2016-07-16 11:03:12 -0400

    fix1 for temperature conversion.

--- temperature.js
+++ temperature.js
@@ -8,7 +8,8 @@ board.on("ready", function() {
     // Assuming a sensor is attached to pin "A0"
     this.pinMode(0, five.Pin.ANALOG);
     this.analogRead(0, function(value) {
-        var celsiusValue = value * 330 / 1024;
+        var voltage = value * (3300 / 1024);
+        var celsiusValue = (voltage - 500) / 10;
         var farenValue = celsiusValue * 9 / 5 + 32;
         console.log("Analog: " + value + " , C: " + celsiusValue.toPrecision(3) + ", F: " + farenValue);
     });
