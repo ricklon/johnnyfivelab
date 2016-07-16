9784d6a13d208cd12568a9075505c56cd210580b
ricklon <rick.rickanderson@gmail.com>
2016-07-16 14:21:36 -0400

    fixed voltage variable error.

--- server-io.js
+++ server-io.js
@@ -116,7 +116,7 @@ board.on("ready", function(socket) {
         // When the sensor value changes, log the value
         tempSensor.on("change", function(value) {
             var tempVoltage =  value * 3300 / 1024;
-            var celsiusValue = (voltage - 500) / 10;
+            var celsiusValue = (tempVoltage - 500) / 10;
             var farenValue = celsiusValue * 9 / 5 + 32;
             console.log("Analog: " + value + " , C: " + celsiusValue.toPrecision(3) + ", F: " + farenValue);
             socket.emit('sendTemp', {
