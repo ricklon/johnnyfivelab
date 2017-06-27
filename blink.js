745249d00b1f4d64c67213c7fb9863e1de6a3cd0
Rick Anderson <rick.rickanderson@gmail.com>
2017-06-27 14:13:39 -0400

    fixed pin number

--- blink.js
+++ blink.js
@@ -1,10 +1,10 @@
 var config = require('config');
 var five = require("johnny-five");
 var board = new five.Board({port: config.get('port')});
-var PIN_LED1 = 21; //0
+var PIN_LED1 = 1; // Fubarino_mini pin 1
 
 board.on("ready", function() {
-  // Create an Led on pin 0
+  // Create an Led on LED_PIN
   var led = new five.Led(PIN_LED1);
 
   // Strobe the pin on/off, defaults to 100ms phases
