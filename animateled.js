8c1af76b155b5b678a85446f43b4fa711a1bb8ca
Rick Anderson <rick.rickanderson@gmail.com>
2017-07-04 19:29:26 -0400

    Cleaned up and organized

--- animateled.js
+++ animateled.js
@@ -1,55 +1,56 @@
-var config = require('config');
-port = config.get('port');
-console.log("port: "+port);
-var Board = require("firmata");
-var board = new Board(port);
-var repl = require('repl');
+const config = require('config');
+const logger = require('winston');
+const Board = require('firmata');
+const repl = require('repl');
 
-//var board = new Board({port: "COM15"});
+const port = config.get('port');
+const board = new Board(port);
 
-var CK_COMMAND = 0x40;
-var CK_PIXEL_SET = 0x10;
-var CK_PIXEL_SHOW = 0x11;
-var CK_PIXEL_CLEAR = 0x12;
-var CK_PIXEL_BRIGHTNESS = 0x13;
-var PIXEL = 1;
-var PIXEL_MAX = 30;
-var RED = 0;
-var GREEN = 0;
-var BLUE = 0;
+logger.log('info', `port: ${port}`);
+
+const CK_COMMAND = 0x40;
+const CK_PIXEL_SET = 0x10;
+const CK_PIXEL_SHOW = 0x11;
+const CK_PIXEL_CLEAR = 0x12;
+const CK_PIXEL_BRIGHTNESS = 0x13;
+let PIXEL = 1;
+const PIXEL_MAX = 30;
+let RED = 0;
+let GREEN = 0;
+let BLUE = 0;
 
 repl.start('firmata>').context.board = board;
 
-var packColor = function(red, green, blue) {
-    red &= 0xFF;
-    green &= 0xFF;
-    blue &= 0xFF;
-    var b1 = red >> 1;
-    var b2 = ((red & 0x01) << 6) | (green >> 2);
-    var b3 = ((green & 0x03) << 5) | (blue >> 3);
-    var b4 = (blue & 0x07) << 4;
-    var data = [b1, b2, b3, b4];
-    console.log(data);
-    return data;
+const packColor = function(red, green, blue) {
+  red &= 0xFF;
+  green &= 0xFF;
+  blue &= 0xFF;
+  const b1 = red >> 1;
+  const b2 = ((red & 0x01) << 6) | (green >> 2);
+  const b3 = ((green & 0x03) << 5) | (blue >> 3);
+  const b4 = (blue & 0x07) << 4;
+  const data = [b1, b2, b3, b4];
+  logger.log('info', data);
+  return data;
 };
 
 board.on('ready', function() {
-    console.log("START STRIP");
-    // board.sysexCommand([CK_COMMAND, CK_PIXEL_CLEAR]);
-    // board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
-    // board.sysexCommand([CK_COMMAND]);
+  logger.log('info', 'START STRIP');
+  // board.sysexCommand([CK_COMMAND, CK_PIXEL_CLEAR]);
+  // board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
+  // board.sysexCommand([CK_COMMAND]);
 
-    setInterval(function() {
-        board.sysexCommand([CK_COMMAND, CK_PIXEL_CLEAR]);
-        board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
-        board.sysexCommand([CK_COMMAND, CK_PIXEL_SET, PIXEL++].concat(packColor(RED, GREEN, BLUE)));
-        board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
-        console.log(PIXEL);
-        if (GREEN < 255) {
-            GREEN++;
-        }
-        if (PIXEL > PIXEL_MAX) {
-            PIXEL = 0;
-        }
-    }, 25);
+  setInterval(function() {
+    board.sysexCommand([CK_COMMAND, CK_PIXEL_CLEAR]);
+    board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
+    board.sysexCommand([CK_COMMAND, CK_PIXEL_SET, PIXEL++].concat(packColor(RED, GREEN, BLUE)));
+    board.sysexCommand([CK_COMMAND, CK_PIXEL_SHOW]);
+    logger.log('info', PIXEL);
+    if (GREEN < 255) {
+      GREEN++;
+    }
+    if (PIXEL > PIXEL_MAX) {
+      PIXEL = 0;
+    }
+  }, 25);
 });
