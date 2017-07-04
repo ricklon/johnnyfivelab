6830de4bfa7afacea8f37ac2cd90881ba4de5d21
Rick Anderson <rick.rickanderson@gmail.com>
2017-07-04 12:47:29 -0400

    code clean up

--- button.js
+++ button.js
@@ -1,35 +1,29 @@
-var config = require('config');
-var five = require("johnny-five");
-var board = new five.Board({port: config.get('port')});
-var PIN_LED1 = 1;
-var PIN_BTN1 = 16;
+const config = require('config');
+const logger = require('winston');
+const five = require('johnny-five');
+const board = new five.Board({
+  port: config.get('port'),
+});
+const PIN_BTN1 = 16;
 
-
-board.on("ready", function() {
-
-    var button = new five.Button({
-        pin: PIN_BTN1,
-        invert: true
-    });
-
-    board.repl.inject({
-        button: button
-    });
-
-    // "down" the button is pressed
-    button.on("down", function() {
-        console.log("down");
-    });
-
-    // "hold" the button is pressed for specified time.
-    //        defaults to 500ms (1/2 second)
-    //        set
-    button.on("hold", function() {
-        console.log("hold");
-    });
-
-    // "up" the button is released
-    button.on("up", function() {
-        console.log("up");
-    });
+board.on('ready', function() {
+  const button = new five.Button({
+    pin: PIN_BTN1,
+    invert: true,
+  });
+  // Make button accessible from the command line
+  board.repl.inject({ button, button });
+  // 'down' event the button is pressed
+  button.on('down', function() {
+    logger.log('info', 'down');
+  });
+  // 'hold' event the button is pressed for specified time.
+  //        defaults to 500ms (1/2 second)
+  button.on('hold', function() {
+    logger.log('info', 'hold');
+  });
+  // 'up' event the button is released
+  button.on('up', function() {
+    logger.log('info', 'up');
+  });
 });
