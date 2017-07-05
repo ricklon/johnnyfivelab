8284414002aeb0016037369b9775471663d1b525
Rick Anderson <rick.rickanderson@gmail.com>
2017-07-04 22:00:38 -0400

    added strobe and exit and close

--- helloboard.js
+++ helloboard.js
@@ -9,26 +9,19 @@ const PIN_LED1 = 1; // Fubarino_mini pin 1
 
 logger.log('info', 'johhnyfive: start');
 
-
 board.on('ready', function() {
   //Create an Led on LED_PIN
   const led = new five.Led(PIN_LED1);
-  // Strobe the pin on/off, defaults to 100ms phases
-  led.on();
-
-  board.on('exit', function() {
-    led.off();
-    logger.log('info', 'Board exit')
-  });
+  led.strobe();
   board.repl.inject({
     led: led,
   });
+  board.on('exit', function() {
+    led.off();
+    logger.log('info', 'Board exit');
+  });
 });
-
 board.on('close', function() {
   logger.log('info', 'Board closed')
-});
-board.on('fail', function(event) {
-  logger.log('error', "%s sent a 'fail' message: %s", event.class, event.message);
 });
 logger.log('info', 'johhnyfive: end');
