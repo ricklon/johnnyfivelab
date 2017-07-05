aed1d47717ed63c1839671db034f28a95f0be016
rianders <rianders@docs.rutgers.edu>
2017-07-05 10:31:15 -0400

    updated so configuration has ip and port information.

---
+++ listIpAddress.js
@@ -0,0 +1,44 @@
+let config = require('config');
+const fs = require('fs');
+const os = require('os');
+const prompt = require('prompt');
+
+const ifaces = os.networkInterfaces();
+let adresses = [];
+
+console.log('Current IP Address: %s', config.get('ip'));
+prompt.start();
+// list ip addresses:
+console.log('Select IP Address by typing it\'s number into the prompt.');
+
+Object.keys(ifaces).forEach(function(ifname) {
+  let alias = 0;
+
+  ifaces[ifname].forEach(function(iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(`(${alias}) ${ifname} : ${iface.address}`);
+      adresses.push(iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(`(${alias}) ${ifname} : ${iface.address}`);
+      adresses.push(iface.address);
+    }
+    prompt.get(['num'], function(err, result) {
+      console.log('IP Selected: %s', adresses[result.num]);
+      console.dir(config);
+      config.ip = adresses[result.num];
+      fs.writeFile("./config/default.json", JSON.stringify(config), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+      alias += 1;
+    });
+  });
+});
--- listSerialPorts.js
+++ listSerialPorts.js
@@ -1,26 +1,30 @@
-var config = require("config");
-var fs = require("fs");
-var serialport = require('serialport');
-var prompt = require('prompt');
-var SerialPort = serialport.SerialPort;
-var portName;
+let config = require('config');
+const fs = require('fs');
+const serialport = require('serialport');
+const prompt = require('prompt');
+//const SerialPort = serialport.SerialPort;
+let portName;
 
-console.log("Current port: %s", config.get('port'));
+if (config.has('port')) {
+  console.log('Current port: %s', config.get('port'));
+} else {
+  console.log('No port selected yet.');
+}
 prompt.start();
 // list serial ports:
-console.log("Select serial port by typing it's number into the prompt.");
+console.log('Select serial port by typing it\'s number into the prompt.');
 serialport.list(function(err, ports) {
-    ports.forEach(function(port, ii) {
-        console.log("(%d): %s", ii, port.comName);
-    });
-    prompt.get(['num'], function(err, result) {
-        console.log("Port Selected: %s", ports[result.num].comName);
-        cfg = {port : ports[result.num].comName};
-	fs.writeFile("./config/default.json",JSON.stringify(cfg), function(err) {
-    if(err) {
+  ports.forEach(function(port, ii) {
+    console.log('(%d): %s', ii, port.comName);
+  });
+  prompt.get(['num'], function(err, result) {
+    console.log('Port Selected: %s', ports[result.num].comName);
+    config.port = ports[result.num].comName;
+    fs.writeFile('./config/default.json', JSON.stringify(config), function(err) {
+      if (err) {
         return console.log(err);
-    }
+      }
     });
-    //console.log("Port Selected: %s, ", portName);
-});
+    // console.log('Port Selected: %s, ', portName);
+  });
 });
